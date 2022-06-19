b816ee005348cf7113e2bc432309cbaba3535711
Oscar López <oscar@bitso.com>
2022-06-19 13:58:15 -0500

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState(['Make a react firebase project', 'Record a coding video']);
+  const [input, setInput] = useState('');
+
+  const addTodo = e => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO React Firebase</h1>
+      <form>
+        <input value={input} onChange={e => setInput(e.target.value)} />
+        <button type='Submit' onClick={addTodo}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map(todo => <li>{todo}</li>)}
+      </ul>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClowyp3KPdLAHN7-A9YY4bzQ2UpRY8Mbc",
+    authDomain: "todo-react-firebase-4c59d.firebaseapp.com",
+    projectId: "todo-react-firebase-4c59d",
+    storageBucket: "todo-react-firebase-4c59d.appspot.com",
+    messagingSenderId: "623208414365",
+    appId: "1:623208414365:web:bd713e1d4f00266c3ca874"
+  };
